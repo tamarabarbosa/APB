e817a5ba52f4d781537bee39089a60fb678f1720
Guilherme da Luz <guilherme.daluz@outlook.com>
2013-06-20 15:01:18 -0300

    Refatoração da interface CadastrarServico

--- APB/src/view/CadastrarServico.java
+++ APB/src/view/CadastrarServico.java
@@ -1,15 +1,20 @@
 package view;
 
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JScrollPane;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 @SuppressWarnings("serial")
 public class CadastrarServico extends JFrame {
@@ -31,32 +36,43 @@ public class CadastrarServico extends JFrame {
 	}
 
 	public CadastrarServico() {
-		setTitle("Servi\u00E7o");
+		setTitle("Servi\u00E7os Cadastrados");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 756, 543);
+		setBounds(100, 100, 500, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 11, 506, 482);
+		scrollPane.setBounds(10, 21, 360, 230);
 		contentPane.add(scrollPane);
-
+		
 		table = new JTable();
-		table.setModel(new DefaultTableModel(new Object[][] {}, new String[] {
-				"Servi\u00E7o", "Realizado por", "Valor", "Data" }) {
-			boolean[] columnEditables = new boolean[] { false, false, false,
-					false };
-
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Servi\u00E7o", "Realizado por", "Valor", "Data"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				true, true, true, false
+			};
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
 			}
 		});
+		table.getColumnModel().getColumn(1).setPreferredWidth(88);
+		table.getColumnModel().getColumn(2).setPreferredWidth(59);
 		scrollPane.setViewportView(table);
 
-		JButton botaoNovoServico = new JButton("Novo");
-		botaoNovoServico.addMouseListener(new MouseAdapter() {
+		JButton btnNovoContato = new JButton("Novo");
+		btnNovoContato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNovoContato.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				dispose();
@@ -65,32 +81,37 @@ public class CadastrarServico extends JFrame {
 				frame.setLocationRelativeTo(null);
 			}
 		});
-		botaoNovoServico.setBounds(526, 11, 204, 43);
-		contentPane.add(botaoNovoServico);
+		btnNovoContato.setBounds(380, 24, 94, 23);
+		contentPane.add(btnNovoContato);
 
-		JButton botaoAlterarServico = new JButton("Alterar");
-		botaoAlterarServico.setBounds(526, 65, 204, 43);
-		contentPane.add(botaoAlterarServico);
-
-		JButton botaoRemoverServico = new JButton("Remover");
-		botaoRemoverServico.setBounds(526, 119, 204, 43);
-		contentPane.add(botaoRemoverServico);
-
-		JButton botaoConcluir = new JButton("Voltar");
-		botaoConcluir.addMouseListener(new MouseAdapter() {
+		JButton btnPesquisarContato = new JButton("Pesquisar");
+		btnPesquisarContato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnPesquisarContato.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				dispose();
+				PesquisarServico frame = new PesquisarServico();
+				frame.setVisible(true);
+				frame.setLocationRelativeTo(null);
+			}
+		});
+		btnPesquisarContato.setBounds(380, 58, 94, 23);
+		contentPane.add(btnPesquisarContato);
+
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
 				dispose();
 				TelaOpcoes frame = new TelaOpcoes();
 				frame.setVisible(true);
 				frame.setLocationRelativeTo(null);
 			}
 		});
-		botaoConcluir.setBounds(526, 450, 204, 43);
-		contentPane.add(botaoConcluir);
-
-		JButton botaoPesquisarServico = new JButton("Pesquisar");
-		botaoPesquisarServico.setBounds(526, 173, 204, 43);
-		contentPane.add(botaoPesquisarServico);
+		btnVoltar.setBounds(380, 228, 94, 23);
+		contentPane.add(btnVoltar);
 	}
 }
