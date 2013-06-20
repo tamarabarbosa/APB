4d86ba789b021606a9f4bd7ffbcc127acd2c59be
Thabata Helen Macedo Granja <thabata.helen@gmail.com>
2013-06-20 12:55:23 -0300

    Consertando erro no cadastro da agenda

--- APB/src/dao/AgendaDAO.java
+++ APB/src/dao/AgendaDAO.java
@@ -25,7 +25,7 @@ public class AgendaDAO {
 		this.updateQuery("INSERT INTO " +
 				"Agenda (nome, telefone, descricao) VALUES (" +
 				"\"" + agenda.getNome() + "\", " +
-				"\"" + agenda.getTelefone() + "\" +" +
+				"\"" + agenda.getTelefone() + "\", " +
 				"\"" + agenda.getDescricao() + "\"); "
 			);
 	}
