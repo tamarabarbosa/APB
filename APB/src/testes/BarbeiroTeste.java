62d7920128c13bd3a6ee358d6643ac0655bfc940
AlessandroCaetano <darkbubu123@gmail.com>
2013-07-01 22:05:34 -0300

    Testes finais Para Barbeiro

--- APB/src/testes/BarbeiroTeste.java
+++ APB/src/testes/BarbeiroTeste.java
@@ -1,13 +1,22 @@
 package testes;
 
 import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dao.FactoryConnection;
 import exception.BarbeiroException;
 import model.Barbeiro;
 import org.junit.Test;
 
+import control.BarbeiroController;
+
 public class BarbeiroTeste {
 
 	Barbeiro barbeiro = new Barbeiro();
+	private Connection connection;
 
 	@Test(expected = NullPointerException.class)
 	public void barbeiroNaoPodePossuirNomeNuloPassandoPeloSetter() {
@@ -109,16 +118,6 @@ public class BarbeiroTeste {
 	}
 
 	@Test(expected = AssertionError.class)
-	public void nomeInvalido() {
-		try {
-			barbeiro.setNome("M4R14");
-			fail();
-		} catch (BarbeiroException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test(expected = AssertionError.class)
 	public void cpfNaoPodePassarQuandoInvalido() {
 		Barbeiro barbeiro = new Barbeiro();
 		try {
@@ -159,7 +158,7 @@ public class BarbeiroTeste {
 	}
 
 	@Test(expected = AssertionError.class)
-	public void numeroDaCadeiraNaoPoseSerUmaLetra() {
+	public void numeroDaCadeiraNaoPodeSerUmaLetra() {
 		Barbeiro barbeiro = new Barbeiro();
 
 		try {
@@ -180,6 +179,114 @@ public class BarbeiroTeste {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void umBarbeiroDeveSerCadastrado() {
+		Barbeiro barbeiro = new Barbeiro();
+		String nome = null;
+		try {
+			barbeiro.setNome("Alessandro");
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			barbeiro.setCpf("02919594150");
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			barbeiro.setRg("418757896");
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			barbeiro.setTelefone("3389-9085");
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		BarbeiroController barbeiroController = BarbeiroController
+				.getInstance();
+		try {
+			barbeiroController.inserir(barbeiro);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			connection = FactoryConnection.getInstance().getConnection();
+			ResultSet rs = connection.createStatement().executeQuery(
+					"SELECT nome FROM barbeiro WHERE nome LIKE"
+							+ barbeiro.getNome());
+			nome = rs.getString("nome");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		assertEquals(nome, nome);
+	}
+	
+	@Test
+	public void umBarbeiroDeveSerAlterado() {
+		Barbeiro barbeiro = new Barbeiro();
+		String nome = null;
+		try {
+			barbeiro.setNome("Luciano");
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			barbeiro.setCpf("02919594150");
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			barbeiro.setRg("418757896");
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			barbeiro.setTelefone("3389-9085");
+		} catch (BarbeiroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		BarbeiroController barbeiroController = BarbeiroController
+				.getInstance();
+		try {
+			barbeiroController.excluir(barbeiro);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			connection = FactoryConnection.getInstance().getConnection();
+			ResultSet rs = connection.createStatement().executeQuery(
+					"SELECT nome FROM barbeiro WHERE nome LIKE"
+							+ barbeiro.getNome());
+			nome = rs.getString("nome");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		assertEquals(nome, nome);
 	}
 
 }
