2e2df503c1fc124a39d97c5cf2b207cb37fa069a
Luciano Henrique Nunes de Almeida <mdiebr@gmail.com>
2013-07-04 01:51:02 -0300

    Tela para alterar um serviço selecionado

---
+++ APB/src/view/AlterarServico.java
@@ -0,0 +1,182 @@
+package view;
+
+import java.awt.EventQueue;
+
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import control.ServicoController;
+import exception.ServicoException;
+import model.Servico;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+import dao.FactoryConnection;
+
+@SuppressWarnings("serial")
+public class AlterarServico extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textValor;
+	private JTextField textData;
+	private Connection connection;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AlterarServico frame = new AlterarServico();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public AlterarServico() {
+		setTitle("Cadastrar novo servi\u00E7o");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 474, 214);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblServico = new JLabel("Servi\u00E7o:");
+		lblServico.setBounds(27, 25, 46, 14);
+		contentPane.add(lblServico);
+
+		JLabel lblRealizadoPor = new JLabel("Realizado por:");
+		lblRealizadoPor.setBounds(27, 56, 92, 14);
+		contentPane.add(lblRealizadoPor);
+
+		JLabel lblPreco = new JLabel("Pre\u00E7o (R$):");
+		lblPreco.setBounds(27, 87, 71, 14);
+		contentPane.add(lblPreco);
+
+		JLabel lblData = new JLabel("Data:");
+		lblData.setBounds(267, 87, 46, 14);
+		contentPane.add(lblData);
+
+		textValor = new JTextField();
+		textValor.setBounds(129, 84, 114, 20);
+		contentPane.add(textValor);
+		textValor.setColumns(10);
+
+		textData = new JTextField();
+		textData.setBounds(312, 84, 106, 20);
+		contentPane.add(textData);
+		textData.setColumns(10);
+
+		final JComboBox comboBoxBarbeiro = new JComboBox();
+		comboBoxBarbeiro.setModel(new DefaultComboBoxModel(
+				new String[] { "Selecione um barbeiro" }));
+		comboBoxBarbeiro.setBounds(129, 53, 289, 20);
+		contentPane.add(comboBoxBarbeiro);
+
+		final JComboBox comboBoxServico = new JComboBox();
+		comboBoxServico.setModel(new DefaultComboBoxModel(new String[] {
+				"Selecione um tipo de servi\u00E7o", "Barba", "Corte Adulto",
+				"Corte Infantil" }));
+		comboBoxServico.setMaximumRowCount(4);
+		comboBoxServico.setBounds(129, 22, 289, 20);
+		contentPane.add(comboBoxServico);
+		
+		try {
+			Connection connection = FactoryConnection.getInstance().getConnection();
+			ResultSet rs = connection.createStatement().executeQuery("DELETE FROM servico WHERE "
+							+ "nome = '" + PesquisarServico.getTempNome() + "';");
+			
+				textValor.setText(rs.getString("preco"));
+				textData.setText(rs.getString("data"));
+
+		} catch (SQLException e) {
+			//mostrarMensagemDeErro(e.getMessage());
+		}
+
+
+		JButton botaoSalvar = new JButton("Salvar");
+		botaoSalvar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				try {
+					Servico servico = new Servico();
+					servico.setNomeBarbeiro(comboBoxBarbeiro.getSelectedItem()
+							.toString());
+					servico.setNome(comboBoxServico.getSelectedItem()
+							.toString());
+					servico.setPreco(textValor.getText());
+					servico.setData(textData.getText());
+
+					if (comboBoxServico.getSelectedIndex() == 0) {
+
+						JOptionPane.showMessageDialog(null,
+								"Você deve selecionar um tipo de serviço.");
+
+					} else if (comboBoxBarbeiro.getSelectedIndex() == 0) {
+
+						JOptionPane.showMessageDialog(null,
+								"Você deve selecionar um barbeiro.");
+
+					} else {
+
+						ServicoController servicoController = ServicoController
+								.getInstance();
+						servicoController.alterar(servico);
+
+						JOptionPane.showMessageDialog(null, "Servico "
+								+ comboBoxServico.getSelectedItem().toString()
+								+ " alterado com sucesso");
+
+						comboBoxBarbeiro.setSelectedIndex(0);
+						comboBoxServico.setSelectedIndex(0);
+
+						textValor.setText("");
+						textData.setText("");
+
+					}
+
+				} catch (ServicoException e) {
+					mostrarMensagemDeErro(e.getMessage());
+				} catch (SQLException e1) {
+					mostrarMensagemDeErro(e1.getMessage());
+				}
+			}
+
+			private void mostrarMensagemDeErro(String informacao) {
+				JOptionPane.showMessageDialog(null, informacao, "Atenção",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		botaoSalvar.setBounds(129, 129, 89, 23);
+		contentPane.add(botaoSalvar);
+
+		JButton botaoVoltar = new JButton("Voltar");
+		botaoVoltar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				dispose();
+				PesquisarServico frame = new PesquisarServico();
+				frame.setVisible(true);
+				frame.setLocationRelativeTo(null);
+			}
+		});
+		botaoVoltar.setBounds(253, 129, 89, 23);
+		contentPane.add(botaoVoltar);
+	}
+	
+}
