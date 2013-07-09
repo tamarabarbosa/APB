a56b2d5b807686ffa8222e064bd85a78d644e434
Guilherme da Luz <guilherme.daluz@outlook.com>
2013-07-08 21:21:25 -0300

    Interface Relatório

--- APB/src/view/Relatorios.java
+++ APB/src/view/Relatorios.java
@@ -6,15 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import java.awt.ScrollPane;
+import java.awt.Button;
+import javax.swing.border.TitledBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class Relatorios extends JFrame {
 
-	private JPanel contentPane;
-
 	/**
 	 * Launch the application.
 	 */
+	private JPanel contentPane;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,12 +41,49 @@ public class Relatorios extends JFrame {
 	 * Create the frame.
 	 */
 	public Relatorios() {
+		setTitle("Relat\u00F3rios");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 10, 660, 540);
+		contentPane.add(scrollPane);
+
+		final DefaultTableModel modelo = new DefaultTableModel(null,
+				new String[] { "Barbeiro Responsável", "Tipo de Serviço", "Valor", "Data" }) {
+			boolean[] columnEditables = new boolean[] { false, false, false, false };
+
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		};
+		
+		final JTable table = new JTable(modelo);
+		table.getColumnModel().getColumn(0).setResizable(false);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(2).setResizable(false);
+		table.getColumnModel().getColumn(3).setResizable(false);
+		scrollPane.setViewportView(table);
+		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnPesquisar.setBounds(680, 13, 94, 23);
+		contentPane.add(btnPesquisar);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnVoltar.setBounds(680, 527, 94, 23);
+		contentPane.add(btnVoltar);
+		
+	}
 }
