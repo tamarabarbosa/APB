36e14e3a67c1b838c5f6d0ebf7effbf1b5655457
Guilherme da Luz <guilherme.daluz@outlook.com>
2013-07-10 13:17:40 -0300

    Metodo para converter data para o formato ISO

--- APB/src/model/Relatorio.java
+++ APB/src/model/Relatorio.java
@@ -1,14 +1,18 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Relatorio {
 
 	private String dataInicial;
 	private String dataFinal;
 	private String barbeiro;
 	private String tipoServico;
-	
-	
-	
+	private SimpleDateFormat formatoDaData;
+	private Date data;
+
 	public Relatorio(String dataInicial, String dataFinal, String barbeiro,
 			String tipoServico) {
 		super();
@@ -17,31 +21,55 @@ public class Relatorio {
 		this.barbeiro = barbeiro;
 		this.tipoServico = tipoServico;
 	}
-	
-	private Relatorio() {
+
+	public Relatorio() {
 	}
-	
+
 	public String getDataInicial() {
 		return dataInicial;
 	}
+
 	public void setDataInicial(String dataInicial) {
-		this.dataInicial = dataInicial;
+
+		formatoDaData = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			data = formatoDaData.parse(dataInicial);
+			this.dataInicial = (formatoDaData.format(data));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			this.dataInicial = ("Parse Date Error");
+		}
 	}
+
 	public String getDataFinal() {
 		return dataFinal;
 	}
+
 	public void setDataFinal(String dataFinal) {
-		this.dataFinal = dataFinal;
+		formatoDaData = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			data = formatoDaData.parse(dataFinal);
+			this.dataFinal = (formatoDaData.format(data));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			this.dataFinal = ("Parse Date Error");
+		}
 	}
+
 	public String getBarbeiro() {
 		return barbeiro;
 	}
+
 	public void setBarbeiro(String barbeiro) {
 		this.barbeiro = barbeiro;
 	}
+
 	public String getTipoServico() {
 		return tipoServico;
 	}
+
 	public void setTipoServico(String tipoServico) {
 		this.tipoServico = tipoServico;
 	}
