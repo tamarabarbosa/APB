cb9b5871f65789c191f941d4cae5f40d79db4f0c
AlessandroCaetano <darkbubu123@gmail.com>
2013-07-11 21:32:29 -0300

    Erro caso barbeiro não foi selecionado no alterar

--- APB/src/view/AlterarBarbeiro.java
+++ APB/src/view/AlterarBarbeiro.java
@@ -144,6 +144,8 @@ public class AlterarBarbeiro extends JFrame {
 					CadastrarBarbeiro frame = new CadastrarBarbeiro();
 					frame.setVisible(true);
 					frame.setLocationRelativeTo(null);
+				} catch (ArrayIndexOutOfBoundsException e1) {
+					mostrarMensagemDeErro("Selecione um Barbeiro para alterar");
 				} catch (BarbeiroException e1) {
 					mostrarMensagemDeErro(e1.getMessage());
 				} catch (SQLException k) {
