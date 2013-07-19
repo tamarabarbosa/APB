308fdcc01679e28b6b1ec695b629663d40e4a06b
LucianoAlmeida <mdiebr@gmail.com>
2013-07-19 14:57:09 -0300

    Gráfico de serviços prestados no por dia

--- APB/src/view/VisualizarRelatorios.java
+++ APB/src/view/VisualizarRelatorios.java
@@ -2,42 +2,6 @@ package view;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JButton;
-
-import model.Relatorio;
-
-import control.BarbeiroController;
-import control.RelatorioController;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-
-import view.PesquisarRelatorio;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
-
-import exception.RelatorioException;
-
 @SuppressWarnings("serial")
 public class VisualizarRelatorios extends JFrame {
 
@@ -47,6 +11,7 @@ public class VisualizarRelatorios extends JFrame {
 	private JPanel contentPane;
 	private double total = 0;
 	private String numero;
+	private int j = 0;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -76,7 +41,7 @@ public class VisualizarRelatorios extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JScrollPane scrollPane = new JScrollPane();
+		final JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 10, 660, 508);
 		contentPane.add(scrollPane);
 
@@ -282,45 +247,67 @@ public class VisualizarRelatorios extends JFrame {
 		contentPane.add(painelGrafico);
 		painelGrafico.setVisible(true);
 
+		if (PesquisarRelatorio.tipoBusca != 0) {
+			try {
+				CategoryDataset cds;
+				cds = createDatasetRelatorio();
+				String titulo = "Total Por Dia";
+				String eixoy = "Valores";
+				String txt_legenda = "Ledenda:";
+				boolean legenda = true;
+				boolean tooltips = true;
+				boolean urls = true;
+				JFreeChart graf = ChartFactory.createBarChart(titulo,
+						txt_legenda, eixoy, cds, PlotOrientation.VERTICAL,
+						legenda, tooltips, urls);
+				ChartPanel myChartPanel = new ChartPanel(graf, true);
+				myChartPanel.setSize(painelGrafico.getWidth(),
+						painelGrafico.getHeight());
+				myChartPanel.setVisible(true);
+				painelGrafico.removeAll();
+				painelGrafico.add(myChartPanel);
+				painelGrafico.revalidate();
+				painelGrafico.repaint();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (RelatorioException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
 		JButton btnGrafico = new JButton("Gr\u00E1fico");
 		btnGrafico.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				try {
-					CategoryDataset cds;
-					cds = createDataset();
-					String titulo = "Gráfico de Teste";
-					String eixoy = "Valores";
-					String txt_legenda = "Ledenda:";
-					boolean legenda = true;
-					boolean tooltips = true;
-					boolean urls = true;
-					JFreeChart graf = ChartFactory.createBarChart3D(titulo,
-							txt_legenda, eixoy, cds, PlotOrientation.VERTICAL,
-							legenda, tooltips, urls);
-					ChartPanel myChartPanel = new ChartPanel(graf, true);
-					myChartPanel.setSize(painelGrafico.getWidth(),
-							painelGrafico.getHeight());
-					myChartPanel.setVisible(true);
-					painelGrafico.removeAll();
-					painelGrafico.add(myChartPanel);
-					painelGrafico.revalidate();
-					painelGrafico.repaint();
+				if (PesquisarRelatorio.tipoBusca != 0) {
 					painelGrafico.setVisible(true);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} catch (RelatorioException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					scrollPane.setVisible(false);
+				} else {
+					JOptionPane
+							.showMessageDialog(null,
+									"Você deve fazer uma busca para visualizar o gráfico.");
 				}
-
 			}
 		});
-		btnGrafico.setBounds(680, 86, 94, 54);
+		btnGrafico.setBounds(680, 159, 94, 62);
 		contentPane.add(btnGrafico);
+
+		JButton btnTabela = new JButton("Tabela");
+		btnTabela.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				painelGrafico.setVisible(false);
+				scrollPane.setVisible(true);
+			}
+		});
+		btnTabela.setBounds(680, 86, 94, 62);
+		contentPane.add(btnTabela);
 	}
 
-	private CategoryDataset createDataset() throws SQLException,
+	private CategoryDataset createDatasetRelatorio() throws SQLException,
 			RelatorioException {
 
 		// Criar relatorio
@@ -330,38 +317,60 @@ public class VisualizarRelatorios extends JFrame {
 		relatorio.setDataFinal(PesquisarRelatorio.dataFinal);
 		relatorio.setDataInicial(PesquisarRelatorio.dataInicial);
 
-		// Obter todos os barbeiros cadastrados
-		BarbeiroController barbeiroController = BarbeiroController
+		RelatorioController relatorioController = RelatorioController
 				.getInstance();
-		ResultSet rs = barbeiroController.pesquisar();
+		ResultSet rs = null;
 
-		// Preencher arraylist com todos os barbeiros cadastrados
-		List<String> barbeiros = new ArrayList<String>();
-		while (rs.next()) {
-			barbeiros.add(rs.getString("nome"));
+		if (PesquisarRelatorio.tipoBusca != 0) {
+			if (PesquisarRelatorio.tipoBusca == 1)
+				rs = relatorioController.getInstance().pesquisarPorBarbeiro(
+						relatorio);
+			if (PesquisarRelatorio.tipoBusca == 2)
+				rs = relatorioController.getInstance()
+						.pesquisarPorBarbeiroEServico(relatorio);
+			if (PesquisarRelatorio.tipoBusca == 3)
+				rs = relatorioController.getInstance()
+						.pesquisarPorDataEBarbeiro(relatorio);
+			if (PesquisarRelatorio.tipoBusca == 4)
+				rs = relatorioController.getInstance()
+						.pesquisarPorDataBarbeiroEServico(relatorio);
+			if (PesquisarRelatorio.tipoBusca == 5)
+				rs = relatorioController.getInstance().pesquisarPorServico(
+						relatorio);
+			if (PesquisarRelatorio.tipoBusca == 6)
+				rs = relatorioController.getInstance()
+						.pesquisarPorDataEServico(relatorio);
+			if (PesquisarRelatorio.tipoBusca == 7)
+				rs = relatorioController.getInstance().pesquisarPorData(
+						relatorio);
 		}
 
-		rs = RelatorioController.getInstance().pesquisarPorData(relatorio);
+		List<String> dias = new ArrayList<String>();
 
-		double totalServico = 0;
+		while (rs.next()) {
+			if (dias.contains(rs.getString("data")) == false)
+				dias.add(rs.getString("data"));
+		}
+
+		double totalPorDia = 0;
+
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		// Obter o valor total de cada barbeiro
-		for (int i = 0; i < barbeiros.size(); i++) {
+
+		for (int i = 0; i < dias.size(); i++) {
 			rs.beforeFirst();
 			while (rs.next()) {
-				if (rs.getString("barbeiro").equals(barbeiros.get(i))) {
-					totalServico += Double.parseDouble(rs.getString("preco")
+				if (rs.getString("data").equals(dias.get(i))) {
+					totalPorDia += Double.parseDouble(rs.getString("preco")
 							.replace(",", "."));
+					j++;
 				}
+
 			}
 
-			dataset.addValue(totalServico, barbeiros.get(i),
-					PesquisarRelatorio.dataInicial + " - "
-							+ PesquisarRelatorio.dataFinal);
-			totalServico = 0;
+			dataset.addValue(totalPorDia, dias.get(i), dias.get(0)+" - "+ dias.get(dias.size()-1));
+			totalPorDia = 0;
 		}
 
 		return dataset;
-
 	}
 }
