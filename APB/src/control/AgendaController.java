eb2463c355a44870723f671112249f7708b541df
Eduardo <brasil.eduardo1@gmail.com>
2015-03-30 00:53:07 -0300

    fechando os else que faltam na classe AgendaContro

--- APB/src/control/AgendaController.java
+++ APB/src/control/AgendaController.java
@@ -14,34 +14,40 @@ public class AgendaController {
 	}
 
 	public boolean incluir(Agenda agenda) throws SQLException {
-		if (agenda == null)
+		if (agenda == null) {
 			return false;
-
-		AgendaDAO.getInstance().incluir(agenda);
-		return true;
+		} else {
+			AgendaDAO.getInstance().incluir(agenda);
+			return true;
+		}
 	}
 
 	public boolean alterar(String nome, Agenda agenda) throws SQLException {
-		if (agenda == null)
+		if (agenda == null) {
 			return false;
-
-		Agenda agenda_alterado = agenda;
-		AgendaDAO.getInstance().alterar(nome, agenda_alterado, agenda);
-		return true;
+		} else {
+			Agenda agenda_alterado = agenda;
+			AgendaDAO.getInstance().alterar(nome, agenda_alterado, agenda);
+			return true;
+		}
 	}
 
 	public boolean excluir(Agenda contato) throws SQLException {
-		if (contato == null)
+		if (contato == null) {
 			return false;
-
-		AgendaDAO.getInstance().excluir(contato);
-		return true;
+		} else {
+			AgendaDAO.getInstance().excluir(contato);
+			return true;
+		}
 	}
 
 	public static AgendaController getInstance() {
-		if (instance == null)
+		if (instance == null) {
 			instance = new AgendaController();
-		return instance;
+			return instance;
+		} else {
+			return instance;
+		}
 	}
 
 	public ResultSet mostrarContatosCadastrados(Agenda contato)
