64368c7fc82261cf88732717f2236286bdaf4677
ThabataG <thabata.helen@gmail.com>
2015-04-05 21:09:23 -0300

    Comment method in class ServicoException

--- APB/src/exception/ServicoException.java
+++ APB/src/exception/ServicoException.java
@@ -3,10 +3,12 @@ package exception;
 @SuppressWarnings("serial")
 public class ServicoException extends Exception {
 
+	// Class constructor
 	public ServicoException() {
 		super();
 	}
 
+	// Method that returns the exception message
 	public ServicoException(String message) {
 		super(message);
 	}
