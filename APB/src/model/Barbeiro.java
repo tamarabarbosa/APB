95156f864d689cf243f8ddd24743cb58c5395d11
Lucas Freitas <lucas_but@hotmail.com>
2015-04-09 01:40:43 -0300

    Changing the language of the variables in the class Barbeiro to english

--- APB/src/model/Barbeiro.java
+++ APB/src/model/Barbeiro.java
@@ -5,26 +5,26 @@ import exception.BarbeiroException;
 public class Barbeiro {
 
 	//Declaration of atributes of the barber
-	private String nome;
-	private String cpf;
-	private String rg;
-	private String telefone;
-	private String cadeira;
+	private String name;
+	private String ir;
+	private String id;
+	private String phoneNumber;
+	private String chair;
 
-	//Declaration of instace variable to the class
-	private static String tempNome;
+	//Declaration of instace variable
+	private static String tempName;
 
 	//Declarion of the constants along de class
-	private final String NOME_INVALIDO = "Nome Inválido";
-	private final String NOME_BRANCO = "Nome em Branco";
-	private final String CPF_INVALIDO = "CPF Inválido";
-	private final String CPF_BRANCO = "CPF em Branco";
-	private final String RG_BRANCO = "RG em Branco";
-	private final String RG_INVALIDO = "RG Inválido";
-	private final String TELEFONE_INVALIDO = "Telefone Inválido";
-	private final String TELEFONE_BRANCO = "Telefone em Branco";
-	private final String CADEIRA_INVALIDA = "Cadeira Inválida";
-	private final String CADEIRA_BRANCO = "Campo Cadeira em Branco";
+	private final String INVALID_NAME = "Nome Inválido";
+	private final String EMPTY_NAME = "Nome em Branco";
+	private final String INVALID_IR = "CPF Inválido";
+	private final String EMPTY_IR = "CPF em Branco";
+	private final String EMPTY_ID = "RG em Branco";
+	private final String INVALID_ID = "RG Inválido";
+	private final String INVALID_PHONE = "Telefone Inválido";
+	private final String EMPTY_PHONE = "Telefone em Branco";
+	private final String INVALID_CHAIR = "Cadeira Inválida";
+	private final String EMPTY_CHAIR = "Campo Cadeira em Branco";
 
 	//Constructor of the barber
 	public Barbeiro() {
@@ -34,41 +34,41 @@ public class Barbeiro {
 	/**	 
 	* Set the name of the new barber in system.
 	*
- 	* @param nome is the name of the barber to be placed into nome. 
- 	* @param cpf is the CPF (Indivdual Registration) of the barber to be placed into cpf.
- 	* @param rg is the ID (Identidade) of the barber to be placed into rg.
- 	* @param telefone is the phone number of the barber to be placed into telefone.
- 	* @param cadeira is the chair that the barber will work, to be placed into cadeira.
+ 	* @param name is the name of the barber to be placed into name. 
+ 	* @param ir is the IR (Indivdual Registration) of the barber to be placed into ir.
+ 	* @param id is the ID (Identidade) of the barber to be placed into id.
+ 	* @param phoneNumber is the phone number of the barber to be placed into phoneNumber.
+ 	* @param chair is the chair that the barber will work, to be placed into chair.
  	* 
  	* @exception BarbeiroException as parameter. 
- 	* @throws IllegalArgumentException if the name didn't be filled. 
- 	* @throws IllegalArgumentException if the CPF (Indivdual Registration) didn't be filled. 
- 	* @throws IllegalArgumentException if the ID didn't be filled. 
- 	* @throws IllegalArgumentException if the phone number didn't be filled. 
- 	* @throws IllegalArgumentException if the chair didn't be filled. 
+ 	* @throws IllegalArgumentException if the name isn't filled. 
+ 	* @throws IllegalArgumentException if the IR (Indivdual Registration) isn't filled. 
+ 	* @throws IllegalArgumentException if the ID isn't filled. 
+ 	* @throws IllegalArgumentException if the phone number isn't filled. 
+ 	* @throws IllegalArgumentException if the chair isn't filled	. 
  	*/
-	public Barbeiro(String nome, String cpf, String rg, String telefone,
-			String cadeira) throws BarbeiroException {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.rg = rg;
-		this.telefone = telefone;
-		this.cadeira = cadeira;
+	public Barbeiro(String name, String ir, String id, String phoneNumber,
+			String chair) throws BarbeiroException {
+		this.name = name;
+		this.ir = ir;
+		this.id = id;
+		this.phoneNumber = phoneNumber;
+		this.chair = chair;
 
-		if (this.nome == null)
-			throw new IllegalArgumentException(NOME_BRANCO);
+		if (this.name == null)
+			throw new IllegalArgumentException(EMPTY_NAME);
 
-		if (this.cpf == null)
-			throw new IllegalArgumentException(CPF_BRANCO);
+		if (this.ir == null)
+			throw new IllegalArgumentException(EMPTY_IR);
 
-		if (this.rg == null)
-			throw new IllegalArgumentException(RG_BRANCO);
+		if (this.id == null)
+			throw new IllegalArgumentException(EMPTY_ID);
 
-		if (this.telefone == null)
-			throw new IllegalArgumentException(TELEFONE_BRANCO);
+		if (this.phoneNumber == null)
+			throw new IllegalArgumentException(EMPTY_PHONE);
 
-		if (this.cadeira == null)
-			throw new IllegalArgumentException(CADEIRA_BRANCO);
+		if (this.chair == null)
+			throw new IllegalArgumentException(EMPTY_CHAIR);
 	}
 
 	/**
@@ -76,26 +76,27 @@ public class Barbeiro {
 	* 
 	* @return current name of the barber in system.
 	*/
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
 	/**
-	* Get the current CPF (Indivdual Registration).
+	* Get the current IR (Indivdual Registration).
 	* 
-	* @return current CPF (Indivdual Registration) of the barber in system.
+	* @return current IR (Indivdual Registration) of the barber in system.
 	*/
-	public String getCpf() {
-		return cpf;
+	public String getIr() {
+		return ir;
 	}
+
 
 	/**
 	* Get the current ID.
 	* 
 	* @return current ID of the barber in system.
 	*/
-	public String getRg() {
-		return rg;
+	public String getId() {
+		return id;
 	}
 
 	/**
@@ -103,8 +104,8 @@ public class Barbeiro {
 	* 
 	* @return current phone number of the barber in system.
 	*/
-	public String getTelefone() {
-		return telefone;
+	public String getPhoneNumber()) {
+		return phoneNumber;
 	}
 
 	/**
@@ -112,29 +113,29 @@ public class Barbeiro {
 	* 
 	* @return current chair of the barber in system.
 	*/
-	public String getCadeira() {
-		return cadeira;
+	public String getChair() {
+		return chair;
 	}
 
 	/**	 
 	* Set the name of the barber to in system, in case of the field name didn't be filled 
 	* the method return an warning while the user doesn't fill correctly.
 	*
- 	* @param nome is the name of the barber to be placed into nome.
+ 	* @param name is the name of the barber to be placed into name.
  	* 
  	* @throws NullPointerException if the name returns a null value.
  	* @throws BarbeiroException if the user didn't fill de name.
  	* @throws BarbeiroException if the name can't be placed in nome.
  	*/
-	public void setNome(String nome) throws BarbeiroException {
-		if (nome == null)
-			throw new NullPointerException(NOME_BRANCO);
-		else if ("".equals(nome))
-			throw new BarbeiroException(NOME_BRANCO);
-		else if (nome.matches("^[[ ]|\\p{L}*]+$"))
-			this.nome= nome;
+	public void setName(String name) throws BarbeiroException {
+		if (name == null)
+			throw new NullPointerException(EMPTY_NAME);
+		else if ("".equals(name))
+			throw new BarbeiroException(EMPTY_NAME);
+		else if (name.matches("^[[ ]|\\p{L}*]+$"))
+			this.name= name;
 		else
-			throw new BarbeiroException(NOME_INVALIDO);
+			throw new BarbeiroException(INVALID_NAME);
 	}
 
 	/**	 
@@ -147,75 +148,75 @@ public class Barbeiro {
  	* @throws BarbeiroException if the user didn't fill de cpf.
  	* @throws BarbeiroException if the IR doesn't match with the valid IR.
  	*/
-	public void setCpf(String cpf) throws BarbeiroException {
-		// Sample of valid CPF: 493.751.185-84
+	public void setIr(String ir) throws BarbeiroException {
+		// Sample of valid IR: 493.751.185-84
 		try {
-			if (cpf == null)
-				throw new NullPointerException(CPF_BRANCO);
+			if (ir == null)
+				throw new NullPointerException(EMPTY_IR);
 			else if ("".equals(cpf))
-				throw new AssertionError(CPF_BRANCO);
-			else if (cpf.matches("[\\d]{3,3}.[\\d]{3,3}.[\\d]{3,3}-[\\d]{2,2}$"))
-				cpf = cpf.split("[\\. | -]")[0] + cpf.split("[\\. | -]")[1]
-				+ cpf.split("[\\. | -]")[2] + cpf.split("[\\. | -]")[3];
-			if (this.validarCpf(cpf))
-				this.cpf = cpf;
+				throw new AssertionError(EMPTY_IR);
+			else if (ir.matches("[\\d]{3,3}.[\\d]{3,3}.[\\d]{3,3}-[\\d]{2,2}$"))
+				ir = ir.split("[\\. | -]")[0] + ir.split("[\\. | -]")[1]
+				+ ir.split("[\\. | -]")[2] + ir.split("[\\. | -]")[3];
+			if (ir.validateIr(ir))
+				this.ir = ir;
 			else
-				throw new BarbeiroException(CPF_INVALIDO);
+				throw new BarbeiroException(INVALID_IR);
 		} catch (AssertionError e) {
-			throw new BarbeiroException(CPF_INVALIDO);
+			throw new BarbeiroException(INVALID_IR);
 		}
 	}
 
 	//this setter define how the RG of barber must be filled
-	public void setRg(String rg) throws BarbeiroException {
-		if (rg == null)
-			throw new NullPointerException(RG_BRANCO);
-		else if ("".equals(rg))
-			throw new BarbeiroException(RG_BRANCO);
-		else if (rg.matches("^[[ ]|\\p{L}*]+$"))
-			throw new AssertionError(RG_INVALIDO);
-		else if (rg.matches("^[0-9]*$"))
-			this.rg = rg;
+	public void setId(String id) throws BarbeiroException {
+		if (id == null)
+			throw new NullPointerException(EMPTY_ID);
+		else if ("".equals(id))
+			throw new BarbeiroException(EMPTY_ID;
+		else if (id.matches("^[[ ]|\\p{L}*]+$"))
+			throw new AssertionError(EMPTY_ID);
+		else if (id.matches("^[0-9]*$"))
+			this.id = id;
 		else
-			throw new AssertionError(RG_INVALIDO);
+			throw new AssertionError(INVALID_ID);
 	}
 
 	//this setter define how the telephone of barber must be filled
-	public void setTelefone(String telefone) throws BarbeiroException {
-		if (telefone == null)
-			throw new NullPointerException(TELEFONE_BRANCO);
-		else if ("".equals(telefone))
-			throw new BarbeiroException(TELEFONE_BRANCO);
-		else if (telefone.matches("(\\([\\d]{2,3}\\))?[ ]*[\\d]{4,4}[ ]*-[ ]*[\\d]{4,4}[ ]*$"))
-			this.telefone = telefone;
+	public void setPhoneNumber(String phoneNumber) throws BarbeiroException {
+		if (phoneNumber == null)
+			throw new NullPointerException(EMPTY_PHONE);
+		else if ("".equals(phoneNumber))
+			throw new BarbeiroException(EMPTY_PHONE);
+		else if (phoneNumber.matches("(\\([\\d]{2,3}\\))?[ ]*[\\d]{4,4}[ ]*-[ ]*[\\d]{4,4}[ ]*$"))
+			this.phoneNumber = phoneNumber;
 		else
-			throw new AssertionError(TELEFONE_INVALIDO);
+			throw new AssertionError(INVALID_PHONE);
 	}
 
 	//this setter define how the chair of barber must be filled
-	public void setCadeira(String cadeira) throws BarbeiroException {
-		if (cadeira == null)
-			throw new NullPointerException(CADEIRA_BRANCO);
-		else if ("".equals(cadeira))
-			throw new BarbeiroException(CADEIRA_BRANCO);
-		else if ("0".equals(cadeira) || cadeira.matches("^[[ ]|\\p{L}*]+$"))
-			throw new AssertionError(CADEIRA_INVALIDA);
-		else if (cadeira.matches("^[0-9]{0,2}$"))
-			this.cadeira = cadeira;
+	public void setChair(String chair) throws BarbeiroException {
+		if (chair == null)
+			throw new NullPointerException(EMPTY_CHAIR);
+		else if ("".equals(chair))
+			throw new BarbeiroException(EMPTY_CHAIR);
+		else if ("0".equals(chair) || chair.matches("^[[ ]|\\p{L}*]+$"))
+			throw new AssertionError(INVALID_CHAIR);
+		else if (chair.matches("^[0-9]{0,2}$"))
+			this.chair = chair;
 		else
-			throw new BarbeiroException(CADEIRA_INVALIDA);
+			throw new BarbeiroException(INVALID_CHAIR);
 	}
 	
-	public static String getTempNome() {
-		return tempNome;
+	public static String getTempName() {
+		return tempName;
 	}
 
-	public static void setTempNome(String tempNome) {
-		Barbeiro.tempNome = tempNome;
+	public static void setTempName(String tempName) {
+		Barbeiro.tempName = tempName;
 	}
 
 	//this method verify if the CPF was filled correctly 
-	private boolean validarCpf(String cpf) {
+	private boolean validateIr(String ir) {
 		int d1, d2;
 		int digito1, digito2, resto;
 		int digitoCPF;
