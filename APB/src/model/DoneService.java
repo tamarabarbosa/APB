a26fb242c7bb27a6963570f85dd16b512a35ec3e
Lucas Freitas <lucas_but@hotmail.com>
2015-04-14 17:07:22 -0300

    Translating variables to english in DoneService class

--- APB/src/model/DoneService.java
+++ APB/src/model/DoneService.java
@@ -11,114 +11,114 @@ import java.text.SimpleDateFormat;
 
 public class DoneService {
 
-	private String nomeServico;
-	private String nomeBarbeiro;
-	private String preco;
-	private String data;
+	private String serviceName;
+	private String barberName;
+	private String price;
+	private String date;
 
-	private final String NOME_INVALIDO = "Nome do Serviço Inválido";
-	private final String NOME_BRANCO = "Nome do Serviço em Branco";
-	private final String BARBEIRO_INVALIDO = "Nome do Barbeiro em Branco";
-	private final String BARBEIRO_BRANCO = "Insira um Barbeiro responsável pelo serviço";
-	private final String PRECO_INVALIDO = "Preço Inválido";
-	private final String PRECO_BRANCO = "Preço em Branco";
-	private final String DATA_BRANCO = "Data em Branco";
-	private final String DATA_INVALIDA = "Insira uma data válida";
+	private final String INVALID_SERVICE_NAME = "Nome do Serviço Inválido";
+	private final String EMPTY_SERVICE_NAME = "Nome do Serviço em Branco";
+	private final String INVALID_BARBER = "Nome do Barbeiro em Branco";
+	private final String EMPTY_BARBER = "Insira um Barbeiro responsável pelo serviço";
+	private final String INVALID_PRICE = "Preço Inválido";
+	private final String EMPTY_PRICE = "Preço em Branco";
+	private final String EMPTY_DATE = "Data em Branco";
+	private final String INVALID_DATE = "Insira uma date válida";
 
 	public DoneService() {
 
 	}
 
 	// Class constructor
-	public DoneService(String nomeServico, String preco, String nomeBarbeiro) {
-		this.nomeServico = nomeServico;
-		this.preco = preco;
-		this.nomeBarbeiro = nomeBarbeiro;
+	public DoneService(String serviceName, String price, String barberName) {
+		this.serviceName = serviceName;
+		this.price = price;
+		this.barberName = barberName;
 	}
 
-	public String getNomeServico() {
-		return nomeServico;
+	public String getServiceName() {
+		return serviceName;
 	}
 
-	public String getNomeBarbeiro() {
-		return nomeBarbeiro;
+	public String getBarberName() {
+		return barberName;
 	}
 
-	public String getPreco() {
-		return preco;
+	public String getPrice() {
+		return price;
 	}
 
-	public String getData() {
-		return data;
+	public String getDate() {
+		return date;
 	}
 
 	// Service name setter
-	public void setNomeServico(String nomeServico) throws ServiceException {
-		if (nomeServico == null)
-			throw new NullPointerException(NOME_BRANCO);
-		else if ("".equals(nomeServico))
-			throw new ServiceException(NOME_BRANCO);
-		else if (nomeServico.matches("^[[ ]|\\p{L}*]+$"))
-			this.nomeServico = nomeServico;
+	public void setServiceName(String serviceName) throws ServiceException {
+		if (serviceName == null)
+			throw new NullPointerException(EMPTY_SERVICE_NAME);
+		else if ("".equals(serviceName))
+			throw new ServiceException(EMPTY_SERVICE_NAME);
+		else if (serviceName.matches("^[[ ]|\\p{L}*]+$"))
+			this.serviceName = serviceName;
 		else
-			throw new ServiceException(NOME_INVALIDO);
+			throw new ServiceException(INVALID_SERVICE_NAME);
 	}
 
 	// Barber name setter
-	public void setNomeBarbeiro(String nomeBarbeiro) throws ServiceException {
-		if (nomeBarbeiro == null)
-			throw new NullPointerException(BARBEIRO_BRANCO);
-		else if ("".equals(nomeBarbeiro))
-			throw new ServiceException(BARBEIRO_BRANCO);
-		else if (nomeBarbeiro.matches("^[[ ]|\\p{L}*]+$"))
-			this.nomeBarbeiro = nomeBarbeiro;
+	public void setBarberName(String barberName) throws ServiceException {
+		if (barberName == null)
+			throw new NullPointerException(EMPTY_BARBER);
+		else if ("".equals(barberName))
+			throw new ServiceException(EMPTY_BARBER);
+		else if (barberName.matches("^[[ ]|\\p{L}*]+$"))
+			this.barberName = barberName;
 		else
-			throw new ServiceException(BARBEIRO_INVALIDO);
+			throw new ServiceException(INVALID_BARBER);
 	}
 
 	// Price setter
-	public void setPreco(String preco) throws ServiceException {
-		if (preco == null)
-			throw new NullPointerException(PRECO_BRANCO);
-		else if ("".equals(preco))
-			throw new ServiceException(PRECO_BRANCO);
-		else if (preco.matches("[\\d]{1,3},[\\d]{1,2}"))
-			this.preco = preco;
+	public void setPrice(String price) throws ServiceException {
+		if (price == null)
+			throw new NullPointerException(EMPTY_PRICE);
+		else if ("".equals(price))
+			throw new ServiceException(EMPTY_PRICE);
+		else if (price.matches("[\\d]{1,3},[\\d]{1,2}"))
+			this.price = price;
 		else
-			throw new ServiceException(PRECO_INVALIDO);
+			throw new ServiceException(INVALID_PRICE);
 	}
 
 	// Date setter
-	public void setData(String data) throws ServiceException, ParseException {
+	public void setDate(String date) throws ServiceException, ParseException {
 
-		if (data == null)
-			throw new NullPointerException(DATA_BRANCO);
-		else if ("".equals(data))
-			throw new ServiceException(DATA_BRANCO);
-		else if (data.matches("[\\d]{1,4}-[\\d]{1,2}-[\\d]{1,2}")) {
-			this.data = data;
-		} else if (data.matches("[\\d]{1,2}/[\\d]{1,2}/[\\d]{1,4}")) {
+		if (date == null)
+			throw new NullPointerException(EMPTY_DATE);
+		else if ("".equals(date))
+			throw new ServiceException(EMPTY_DATE);
+		else if (date.matches("[\\d]{1,4}-[\\d]{1,2}-[\\d]{1,2}")) {
+			this.date = date;
+		} else if (date.matches("[\\d]{1,2}/[\\d]{1,2}/[\\d]{1,4}")) {
 
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date dataIso = sdf.parse(data);
+			Date dateISO = sdf.parse(date);
 
 			SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
-			String dataISO = sdf2.format(dataIso);
-
-			this.data = dataISO;
+			String stringDateBR = sdf2.format(dateISO);
+			
+			this.date = stringDateBR;
 		} else
-			throw new ServiceException(DATA_INVALIDA);
+			throw new ServiceException(INVALID_DATE);
 	}
 
 	// Method to convert service date to ABNT
-	public String ConverterDataParaABNT(String data) throws ParseException {
+	public String ConvertTOABNT(String date) throws ParseException {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date dataISO = sdf.parse(data);
-
+		Date dateISO = sdf.parse(date);
+		
 		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy");
-		String databr = sdf2.format(dataISO);
-
-		return databr;
+		String stringDateBR = sdf2.format(dateISO);
+		
+		return stringDateBR;
 	}
 }
