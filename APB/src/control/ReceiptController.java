511767231de2a461b80a7afb509ae0c112813b01
ThabataG <thabata.helen@gmail.com>
2015-05-12 19:36:21 -0300

    Descomplicate

--- APB/src/control/ReceiptController.java
+++ APB/src/control/ReceiptController.java
@@ -13,32 +13,50 @@ import java.sql.SQLException;
 
 import dao.ReceiptDAO;
 
-;
-
 public class ReceiptController {
 
+	// Stores the current instance of the class
 	private static ReceiptController instance;
 
+	// General class constructor
 	public ReceiptController() {
 	}
 
-	// this check if the instance is there
+	/**
+	 * @return - Return the current instance if exists, or instantiate a new one
+	 *         if does not and return it
+	 */
 	public static ReceiptController getInstance() {
+		// "ReceiptController" Class Instance
 		if (instance == null) {
 			instance = new ReceiptController();
 		} else {
-			/* nothing to do. */
+			// Nothing to do - because the condition "if" is just used to check
+			// the initial value of the variable
 		}
-		return instance;
 
+		return instance;
 	}
 
-	// show results of search of barber service
-	public ResultSet pesquisarJobsDoBarber(String barber,
-			String dataInicial, String dataFinal) throws SQLException {
+	/**
+	 * Method that gives access to the barber services search
+	 * 
+	 * @param barberName
+	 *            - Contains the barber name
+	 * @param initialDate
+	 *            - Contains the initial date
+	 * @param finalDate
+	 *            - Contains the final date
+	 * @return - Return the ResultSet of the search
+	 */
+	public ResultSet barberServicesSearch(String barber, String dataInicial,
+			String dataFinal) throws SQLException {
 
-		return ReceiptDAO.getInstance().pesquisarJobsDoBarber(barber,
-				dataInicial, dataFinal);
+		ReceiptDAO receiptDAOInstance = ReceiptDAO.getInstance();
+		ResultSet barberServicesSearchResult = receiptDAOInstance
+				.barberServicesSearch(barber, dataInicial, dataFinal);
+
+		return barberServicesSearchResult;
 
 	}
 
