57ef3c9e1d9b4f0dd4e5b491d9c728300f5bef26
Eduardo Brasil <brasil.eduardo1@gmail.com>
2015-05-19 21:40:50 -0300

    fix project

--- APB/src/tests/PhonebookTeste.java
+++ APB/src/tests/PhonebookTeste.java
@@ -18,85 +18,85 @@ public class PhonebookTeste {
 	@Before
 	public void setUp(){
 		try {
-			contact.setNome("Alessandro");
-			contact.setPhone("4568-9856");
+			contact.setName("Alessandro");
+			contact.setName("4568-9856");
 		} catch (BarberException e1) {
 			e1.printStackTrace();
 		}
-		contact.setDescricao("ASDAS");
+		contact.setDescription("ASDAS");
 	}
 
 	@Test
 	public void contrutorDePhonebookDeveFuncionar(){
 		Phonebook contact = new Phonebook("Alessandro", "6589-5689", "aaaa");
-		assertEquals("Alessandro", contact.getNome());
-		assertEquals("6589-5689", contact.getPhone());
-		assertEquals("aaaa", contact.getDescricao());
+		assertEquals("Alessandro", contact.getName());
+		assertEquals("6589-5689", contact.getPhoneNumber());
+		assertEquals("aaaa", contact.getDescription());
 	}
 
 	@Test
 	public void getterDeNomeDeveFuncionar(){
-		assertEquals("Alessandro", contact.getNome());
+		assertEquals("Alessandro", contact.getName());
 	}
 
 	@Test
 	public void getterDePhoneDeveFuncionar(){
-		assertEquals("4568-9856", contact.getPhone());
+		assertEquals("4568-9856", contact.getPhoneNumber());
 	}
 
 	@Test
 	public void getterDeDescricaoDeveFuncionar(){
-		assertEquals("ASDAS", contact.getDescricao());
+		assertEquals("ASDAS", contact.getDescription());
 	}
 
 
 	@Test(expected = BarberException.class)
 	public void nameDoBarberNaoPodePassarQuandoEmBranco() throws BarberException{
-		contact.setNome("");
+		contact.setName("");
 		Assert.fail("Deve lançar uma exceção");
 	}
 
 	@Test(expected = BarberException.class)
 	public void telefoneDoBarberNaoPodePassarQuandoEmBranco() throws BarberException{
-		contact.setPhone("");
+		contact.setName("");
 		Assert.fail("Deve lançar uma exceção");
 	}
 
 	@Test(expected = BarberException.class)
 	public void nameDoBarberNaoPodePassarQuandoForaDeFormato() throws BarberException{
-		contact.setNome("ASDAS!!");
+		contact.setName("ASDAS!!");
 		Assert.fail("Deve lançar uma exceção");
 	}
 
 	@Test(expected = BarberException.class)
 	public void telefoneDoBarberNaoPodePassarQuandoForaDeFormato() throws BarberException{
-		contact.setPhone("45645aa-a54654");
+		contact.setName("45645aa-a54654");
 		Assert.fail("Deve lançar uma exceção");
 	}
 
 
 	@Test (expected = AssertionError.class)
 	public void getterDeTempNomeDeveRetornarValorPassado() throws JobException {
-		assertEquals("Barba", Phonebook.getTempNome());
+		assertEquals("Barba", Phonebook.getTempName());
 	}
 
 	@Test (expected = AssertionFailedError.class)
 	public void setterDeTempNomeNaoPodeSerNulo() throws JobException {
-		Phonebook.setTempNome(null);
+		Phonebook.setTempName(null);
 		Assert.fail("Deve lançar exceção");
 	}
 
 
 	@Test (expected = AssertionFailedError.class)
 	public void setterDeTempNomeNaoPodeSerEmBranco() {
-		Phonebook.setTempNome("");
+		Phonebook.setTempName("");
 		Assert.fail("Deve lançar exceção");
 	}
 
 	@Test
 	public void tempNomeValido() throws BarberException {
-		Phonebook.setTempNome("Paulo");
-		assertEquals("Paulo", Phonebook.getTempNome());
+		Phonebook.setTempName("Paulo");
+		assertEquals("Paulo", Phonebook.getTempName());
 	}
 
 }
